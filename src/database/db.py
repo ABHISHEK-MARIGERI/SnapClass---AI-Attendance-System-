c10f3de90f893136a1bc8ad2e7222894a23fd603
Abhishek Marigeri <abhishekmarigeri9676@gmail.com>
2026-07-25 20:11:02 +0530

    Update database functions

--- src/database/db.py
+++ src/database/db.py
@@ -85,3 +85,9 @@ def get_student_subjects(student_id):
 def get_student_attendance(student_id):
     response = supabase.table("attendance_logs").select('*,subjects(*)').eq("student_id",student_id).execute()
     return response.data
+
+
+
+def create_attendence(logs):
+    response = supabase.table("attendance_logs").insert(logs).execute()
+    return response.data
